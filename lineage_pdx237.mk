cee6ba18f509608f0a19094f7e6ffa6902f026cb
kyasu <kyasu.dev@gmail.com>
2026-04-04 15:30:17 +0900

    pdx237: Update blobs to XQ-DE72_Customized_SEA-67.2.A.3.163
    Change-Id: I1671acada6c6fa1bff8642db861d4ab716dbde7d

--- lineage_pdx237.mk
+++ lineage_pdx237.mk
@@ -35,8 +35,8 @@ PRODUCT_MODEL := Xperia 5 V
 PRODUCT_GMS_CLIENTID_BASE := android-sonymobile
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="XQ-DE72-user 15 67.2.A.3.141 067002A003014100523109309 release-keys" \
-    BuildFingerprint=Sony/XQ-DE72/XQ-DE72:15/67.2.A.3.141/067002A003014100523109309:user/release-keys \
+    BuildDesc="XQ-DE72-user 15 67.2.A.3.163 067002A003016300523109309 release-keys" \
+    BuildFingerprint=Sony/XQ-DE72/XQ-DE72:15/67.2.A.3.163/067002A003016300523109309:user/release-keys \
     DeviceName=XQ-DE72 \
     DeviceProduct=XQ-DE72 \
     SystemDevice=XQ-DE72 \
